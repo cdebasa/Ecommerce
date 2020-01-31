eedeef2ef991e511796880bf7242e2e51ed00019
cdebasa <cdebasa@teckpert.com>
2020-01-30 21:28:13 -0500

    broken homepage to smaller components. Finished stylying menu

---
+++ src/page/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import './hompage.styles.scss'
+import Directory from '../../components/diretory/directory.component';
+
+
+const HomePage = () => (
+    <Directory />
+);
+
+export default HomePage;
